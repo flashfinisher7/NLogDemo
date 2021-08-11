b330f6b919bd13a448f88edf4e08a15d83218d43
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-11 15:25:23 +0530

    Refactor:NLog Demo

---
+++ Nlog/Nlog/NLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NLog;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace NlogSession
+{
+   class Nlog
+   {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public void LogDebug(string message)
+       {
+           logger.Debug(message);
+       }
+       public void LogError(string message)
+       {
+           logger.Error(message);
+       }
+       public void LogInfo(String message)
+       {
+           logger.Info(message);
+       }
+       public void LogWarn(String message)
+       {
+           logger.Warn(message);
+       }
+   }
+}
